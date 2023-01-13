b1b6b852ef287d53b4ae9758ad9c9b8a501ea983
Jing Yu Sing <87383042+vongola7262@users.noreply.github.com>
2023-01-13 17:27:12 +0800

    20230103-2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vongola7262.github.io/dist/'
+  publicPath: '/pokeStore_vue/dist/'
 })
